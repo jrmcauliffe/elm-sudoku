1ba7d7f97d99f755829fa1d84ff62b9032a673b8
Jonathan McAuliffe <jon@birchavenue.org>
2022-07-15 12:16:49 +1000

    Basic working board display

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Array exposing (..)
 import Browser
 import Html exposing (Html)
 import Svg exposing (..)
@@ -7,15 +8,15 @@ import Svg.Attributes exposing (..)
 
 
 type alias Model =
-    { dimension : Int
-    , contents : List Int
+    { dim : Int
+    , contents : Array Int
     }
 
 
 initialModel : Model
 initialModel =
-    { dimension = 3
-    , contents = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+    { dim = 2
+    , contents = Array.fromList [ 1, 2, 3, 4, 3, 2, 1, 4, 1, 2, 3, 4, 3, 2, 1, 4 ]
     }
 
 
@@ -42,20 +43,75 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            ( { model | dimension = model.dimension + 1 }, Cmd.none )
+            ( { model | dim = model.dim + 1 }, Cmd.none )
 
         Decrement ->
-            ( { model | dimension = model.dimension - 1 }, Cmd.none )
+            ( { model | dim = model.dim - 1 }, Cmd.none )
+
+
+myNum : Int -> Int -> Int -> Svg Msg
+myNum dim index value =
+    let
+        xOffset =
+            15
+
+        yOffset =
+            38
+
+        (xx,yy) = dimToXY dim index
+
+        row =
+           xx * 50 + xOffset |> String.fromInt
+
+        col =
+           yy * 50 + yOffset |> String.fromInt
+    in
+    text_
+        [ x row
+        , y col
+        , fill "black"
+        , Svg.Attributes.style "font-family: Arial; font-size: 34; stroke: #000000; fill: #000000;"
+        ]
+        [ text (String.fromInt value) ]
+
+
+
+dimToXY : Int -> Int -> (Int, Int)
+dimToXY dim index = (modBy (dim * dim) index, (index // (dim * dim)))
+
+
+myNums : Array Int -> List (Svg Msg)
+myNums nums =
+    let
+        allNums =
+            List.range 0 (Array.length nums - 1)
+                |> List.map (\x -> (nums |> Array.get x) |> Maybe.map (\y -> ( x, y )))
+                |> List.filterMap identity
+    in
+    allNums
+        |> List.map
+            (\( ind, val ) ->
+                myNum 2 ind val
+             --       , Svg.Attributes.style "font-family: Arial; font-size: 34; stroke: #000000; fill: #000000;"
+            )
 
 
 myLines : Int -> Int -> Int -> List (Svg Msg)
 myLines size dim l =
     let
-        sWidth = case (modBy dim l ) of
-            0 -> "4"
-            _ -> "2"
-        lSize = size * l |> String.fromInt
-        bSize = dim * dim * size |> String.fromInt
+        sWidth =
+            case modBy dim l of
+                0 ->
+                    "4"
+
+                _ ->
+                    "2"
+
+        lSize =
+            size * l |> String.fromInt
+
+        bSize =
+            dim * dim * size |> String.fromInt
     in
     [ line
         [ x1 lSize
@@ -79,20 +135,23 @@ myLines size dim l =
         []
     ]
 
-board : Int -> Svg Msg
-board dim =
+
+board : Model -> Svg Msg
+board m =
     let
+        -- edge of box in pixels
         boxSize =
             50
 
+        -- edge of board in pixels
         boardSize =
-            dim * dim * boxSize
+            m.dim * m.dim * boxSize
     in
     g [ transform "translate(100,100)" ]
-        (List.range 1 (dim * dim)
+        (List.range 1 (m.dim * m.dim)
             |> List.concatMap
-                ( myLines boxSize dim
-                )
+                (myLines boxSize m.dim)
+            |> List.append (myNums m.contents)
             |> List.append
                 [ rect
                     [ x "0"
@@ -115,4 +174,4 @@ view model =
         , height "1500"
         , viewBox "0 0 1500 1500"
         ]
-        [ model.dimension |> board ]
+        [ model |> board ]
