1567aacdd296837f491a5aa7103350c29bc7975c
Jonathan McAuliffe <jon@birchavenue.org>
2025-01-11 08:34:27 +1100

    Update test framework to 2.2.0

--- tests/SolverTest.elm
+++ tests/SolverTest.elm
@@ -1,7 +1,7 @@
 module SolverTest exposing (suite)
 
 import Board as B
-import Expect exposing (Expectation)
+import Expect exposing (equal)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Solver
 import Test exposing (..)
@@ -14,27 +14,27 @@ suite =
             -- Nest as many descriptions as you like.
             [ test "Any value is valid in empty board" <|
                 \_ ->
-                    Solver.isValid B.empty ( 1, 1 ) 9 |> Expect.true "Can add any value to empty board"
+                    Solver.isValid B.empty ( 1, 1 ) 9 |> Expect.equal True
             , test "Cannot add existing value to local square" <|
                 \_ ->
                     let
                         b =
                             B.add B.empty ( 1, 1 ) 1
                     in
-                    Solver.isValid b ( 1, 2 ) 1 |> Expect.false "Cannot add existing value to square"
+                    Solver.isValid b ( 1, 2 ) 1 |> Expect.equal False
             , test "Cannot add existing value to the same row" <|
                 \_ ->
                     let
                         b =
                             B.add B.empty ( 1, 1 ) 1
                     in
-                    Solver.isValid b ( 1, 9 ) 1 |> Expect.false "Cannot add existing value to the same row"
+                    Solver.isValid b ( 1, 9 ) 1 |> Expect.equal False
             , test "Cannot add existing value to the same column" <|
                 \_ ->
                     let
                         b =
                             B.add B.empty ( 1, 1 ) 1
                     in
-                    Solver.isValid b ( 9, 1 ) 1 |> Expect.false "Cannot add existing value to the same column"
+                    Solver.isValid b ( 9, 1 ) 1 |> Expect.equal False
             ]
         ]
